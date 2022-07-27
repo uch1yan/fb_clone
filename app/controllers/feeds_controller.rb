2e9cb1aac7ed90c41ac68a051da8346dc70456c9
manami young <manayou1125@outlook.jp>
2022-07-28 06:20:15 +0900

    削除機能の追加

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -26,30 +26,44 @@ class FeedsController < ApplicationController
 
   # POST /feeds or /feeds.json
   def create
-    @feed = current_user.feed.build(feed_params)
-    respond_to do |format|
-      if @feed.save
-        format.html { redirect_to feed_url(@feed), notice: "Feed was successfully created." }
-        format.json { render :show, status: :created, location: @feed }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @feed.errors, status: :unprocessable_entity }
-      end
+    @feed = current_user.feeds.build(feed_params)
+    if params[:back]
+      render :new
+    elsif @feed.save
+      redirect_to feeds_path, notice: '投稿しました！'
+    else
+      render :new
     end
   end
+  #   respond_to do |format|
+  #     if @feed.save
+  #       format.html { redirect_to feed_url(@feed), notice: "Feed was successfully created." }
+  #       format.json { render :show, status: :created, location: @feed }
+  #     else
+  #       format.html { render :new, status: :unprocessable_entity }
+  #       format.json { render json: @feed.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /feeds/1 or /feeds/1.json
   def update
-    respond_to do |format|
-      if @feed.update(feed_params)
-        format.html { redirect_to feed_url(@feed), notice: "Feed was successfully updated." }
-        format.json { render :show, status: :ok, location: @feed }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @feed.errors, status: :unprocessable_entity }
-      end
+    if @feed.update(feed_params)
+      redirect_to feeds_path, notice: '投稿を更新しました！'
+    else
+      render :edit
     end
   end
+  #   respond_to do |format|
+  #     if @feed.update(feed_params)
+  #       format.html { redirect_to feed_url(@feed), notice: "Feed was successfully updated." }
+  #       format.json { render :show, status: :ok, location: @feed }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @feed.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   def confirm
     @feed = current_user.feeds.build(feed_params)
@@ -57,13 +71,17 @@ class FeedsController < ApplicationController
   end
   # DELETE /feeds/1 or /feeds/1.json
   def destroy
-    @feed.destroy
-
-    respond_to do |format|
-      format.html { redirect_to feeds_url, notice: "Feed was successfully destroyed." }
-      format.json { head :no_content }
+    if @feed.destroy
+      redirect_to feeds_path, notice: '投稿を削除しました！'
+    else
+      render :index
     end
   end
+  #   respond_to do |format|
+  #     format.html { redirect_to feeds_url, notice: "Feed was successfully destroyed." }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -73,6 +91,6 @@ class FeedsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def feed_params
-      params.require(:feed).permit(:image, :image_cache)
+      params.require(:feed).permit(:image, :image_cache, :content)
     end
 end
